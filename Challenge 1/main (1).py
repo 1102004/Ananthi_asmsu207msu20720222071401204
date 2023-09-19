ed17077b4f0b79c9682de4bd1cc04009c84c69bf
1102004 <145043824+1102004@users.noreply.github.com>
2023-09-19 21:56:20 +0400

    Add files via upload

---
+++ Challenge 1/main (1).py
@@ -0,0 +1,9 @@
+#1.1 recursive function
+def fact_rect(n):
+if n==0 or n==1:
+  return 1
+else:
+  return n*fact_rec(n-1)
+  number=(int(input("Enter a value:")))
+  res=fact_rec(number)
+  print("The factorial of {} is {}".format(number,res))
